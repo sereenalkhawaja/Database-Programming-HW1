1e554000cdf7d2b973af7609da4141378725e713
sereenalkhawaja <sereenalkhawaja80@gmail.com>
2025-12-04 01:22:11 +0300

    Add files via upload

---
+++ SQLQ2.sql
@@ -0,0 +1,38 @@
+
+-- Name: sereen Alkhawaja
+-- ID: 202220596
+-- Q2 SQL Queries for Bank Database
+
+
+-- A)
+SELECT ID
+FROM depositor 
+EXCEPT
+SELECT ID
+FROM borrower;
+
+
+
+-- B)
+
+SELECT c2.ID
+FROM customer c1
+JOIN customer c2
+    ON c1.customer_street = c2.customer_street
+   AND c1.customer_city = c2.customer_city
+WHERE c1.ID = '12345'
+  AND c2.ID <> '12345';   
+
+
+
+-- C) 
+
+SELECT DISTINCT b.branch_name
+FROM branch b
+JOIN account a
+    ON b.branch_name = a.branch_name
+JOIN depositor d
+    ON a.account_number = d.account_number
+JOIN customer c
+    ON d.ID = c.ID
+WHERE c.customer_city = 'Harrison';
---
+++ SQLQ3.sql
@@ -0,0 +1,34 @@
+-- Name: sereen Alkhawaja
+-- ID: 202220596
+-- Q3 SQL Window Functions
+
+
+--A. Find the cumulative total sum for qty
+-- (Assuming table name is demand)
+
+SELECT
+    day,
+    qty,
+    SUM(qty) OVER (ORDER BY day) AS cumQty
+FROM demand;
+
+
+-- B. Extract the two worst performing days of each product in terms of number of qty sold
+-- (Assuming table name is Product_Sales)
+WITH RankedSales AS (
+    SELECT
+        product,
+        day,
+        qty,
+        RANK() OVER (PARTITION BY product ORDER BY qty ASC) AS RN
+    FROM Product_Sales
+)
+SELECT
+    product,
+    day,
+    qty,
+    RN
+FROM RankedSales
+
+WHERE RN <= 2
+ORDER BY product, RN;
